b4071d8542b734e3cb6724f899f80e62faa2705f
Philip Glebow <pglebow@Phaks-iMac.local>
2017-12-08 17:39:07 -0800

    Init

---
+++ src/main/java/com/glebow/webfluxdemo/WebfluxDemoApplication.java
@@ -0,0 +1,12 @@
+package com.glebow.webfluxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxDemoApplication.class, args);
+	}
+}
